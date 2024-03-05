4ca53626d58726a43f4525d7c4c8f3c1cea02a2f
sonatejasd <sonatejasd@gmail.com>
2024-03-05 14:50:46 +0530

    added modal

--- products.js
+++ products.js
@@ -1,12 +1,6 @@
 let displayFlag=false;
 let productsDiv = document.getElementById("products");
 let productCount = 0;
-let allProductsData;
-
-const productDiv = document.getElementsByClassName("product");
-const modal = document.getElementById("productModal");
-
-
 
 function fetchData() {
   fetch("https://fakestoreapi.com/products")
@@ -14,7 +8,6 @@ function fetchData() {
     return response.json();
   })
   .then(function(products){
-    allProductsData=products;
     getProduct(products)
   })
   .catch(function(){
@@ -26,42 +19,55 @@ fetchData();
 
 function getProduct(products){
   products.forEach(function(product) {
-      const productDiv = document.createElement("div");
-      productDiv.classList.add("product");
-      const productImg = document.createElement("img");
-      productImg.src = product.image;
-      productImg.alt = "product image";
-      productImg.title = product.description;
-      const productTitle = document.createElement("p");
-      productTitle.textContent = product.title;
-      const productPrice = document.createElement("p");
-      productPrice.textContent = "$"+product.price;
-      const addProduct = document.createElement("button");
-      addProduct.textContent = "add to cart";
-      addProduct.addEventListener("click",increment);
-      productDiv.append(productImg, productTitle,productPrice,addProduct);
-      productsDiv.appendChild(productDiv);
+    const productDiv = document.createElement("div");
+    productDiv.classList.add("product");
+    //sub elements of productDiv
+    const productImg = document.createElement("img");
+    productImg.src = product.image;
+    productImg.alt = "product image";
+    productImg.title = product.description;
+    const productTitle = document.createElement("p");
+    productTitle.textContent = product.title;
+    const productPrice = document.createElement("p");
+    productPrice.textContent = "$"+product.price;
+    const addProduct = document.createElement("button");
+    //add to cart button
+    addProduct.textContent = "add to cart";
+    addProduct.addEventListener("click",increment);
+    //modal
+    const modalDiv = document.createElement("div");
+    modalDiv.classList.add("productModal");
+    //add modalDiv children
+    //modalDiv.append(productImg, productTitle, productPrice, addProduct);
+    //add productDiv children
+    productDiv.append(productImg, productTitle, productPrice, addProduct);
+    productsDiv.appendChild(productDiv);
+    productsDiv.appendChild(modalDiv);
+    //view modal
+    productImg.onclick = function(){
+      modalDiv.style.display = "block";
+    }
   });
 }
 
-for(let i=0; i<productDiv; i++){
-  productDiv[i].addEventListener("click",function(){
-    console.log(productData[2].title);
-    modal.style.display = "block";
-    const imageDiv = document.createElement("img");
-    imageDiv.src = product[i].image;
-    const titleDiv = document.createElement("div");
-    titleDiv.textContent = product[i].title;
-    const descriptionDiv = document.createElement("div");
-    descriptionDiv.textContent = product[i].description;
-    const priceDiv = document.createElement("div");
-    priceDiv.textContent = product[i].price;
-    const addProduct = document.createElement("button");
-    addProduct.textContent = "add to cart"
-    const cancelButton = document.createElement("div");
-    modal.append(titleDiv,imageDiv,descriptionDiv,priceDiv,addProduct,cancelButton)
-  });
-}
+// for(let i=0; i<productDiv; i++){
+//   productDiv[i].addEventListener("click",function(){
+//     console.log(productData[2].title);
+//     modal.style.display = "block";
+//     const imageDiv = document.createElement("img");
+//     imageDiv.src = product[i].image;
+//     const titleDiv = document.createElement("div");
+//     titleDiv.textContent = product[i].title;
+//     const descriptionDiv = document.createElement("div");
+//     descriptionDiv.textContent = product[i].description;
+//     const priceDiv = document.createElement("div");
+//     priceDiv.textContent = product[i].price;
+//     const addProduct = document.createElement("button");
+//     addProduct.textContent = "add to cart"
+//     const cancelButton = document.createElement("div");
+//     modal.append(titleDiv,imageDiv,descriptionDiv,priceDiv,addProduct,cancelButton)
+//   });
+// }
 
 
 function increment(){
